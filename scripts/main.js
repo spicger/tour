db002cdf75d93bf96f399873869792274dd08cb9
spicger <spicger0@gmail.com>
2024-05-16 18:04:59 +0500

    swiper section done

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+// Swiper testimonial
+let swiper = new Swiper(".testimonialSwiper", {
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
